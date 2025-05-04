50537dcdc4f4138de16d6363a27ac457b3d34f47
Andrii Prokhorovych <andrii@blockthrough.com>
2025-05-04 18:31:57 +0300

    stage 2

--- app/main.go
+++ app/main.go
@@ -12,6 +12,7 @@ var _ = fmt.Fprint
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }

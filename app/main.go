d4276f7cf59465e2356177ad0438260611a69be8
Andrii Prokhorovych <andrii@blockthrough.com>
2025-05-04 23:24:56 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,14 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		command := strings.Split(input[:len(input)-1], " ")
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		switch command[0] {
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println(command[0] + ": command not found")
+		}
 	}
 }

75d7f7c3db5fc9ef159b972980205a403a4e9ec3
Andrii Prokhorovych <andrii@blockthrough.com>
2025-05-08 18:24:56 +0300

    codecrafters submit [skip ci]

--- app/cmds/type.go
+++ app/cmds/type.go
@@ -2,26 +2,60 @@ package cmds
 
 import (
 	"fmt"
+	"os"
 	"slices"
+	"strings"
 )
 
 type Type struct {
+	osCmds        map[string]string
 	availableCmds []string
 }
 
 func InitType() *Type {
 	availableCmds := []string{"exit", "type", "echo"}
+
+	pathEnv := os.Getenv("PATH")
+	pathArr := strings.Split(pathEnv, ":")
+
+	osCmds := make(map[string]string, 0)
+	for _, dirPath := range pathArr {
+		files, err := os.ReadDir(dirPath)
+		if err != nil {
+			fmt.Errorf("error during reading dir: %v \n", err.Error())
+			continue
+		}
+
+		for _, entry := range files {
+			name := entry.Name()
+			_, ok := osCmds[name]
+			if ok {
+				continue
+			}
+			osCmds[name] = fmt.Sprintf("%s/%s", dirPath, name)
+		}
+	}
+
 	return &Type{
 		availableCmds: availableCmds,
+		osCmds:        osCmds,
 	}
 }
 
 func (t *Type) Run(args []string) {
-	has := slices.Contains(t.availableCmds, args[0])
+	searchableBin := args[0]
+	has := slices.Contains(t.availableCmds, searchableBin)
 	if has {
-		fmt.Printf("%v is a shell builtin \n", args[0])
+		fmt.Printf("%v is a shell builtin \n", searchableBin)
 		return
 	}
 
-	fmt.Printf("%v: not found \n", args[0])
+	val, ok := t.osCmds[searchableBin]
+
+	if !ok {
+		fmt.Printf("%v: not found \n", searchableBin)
+		return
+	}
+
+	fmt.Printf("%s is %s \n", searchableBin, val)
 }
